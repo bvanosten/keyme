226f24e19d55c3900e2bd30dc8fc5f21735bee54
Richard Genther <richard.genthner@wheniwork.com>
2017-03-23 12:58:31 -0400

    bumped version for the recent fixes

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages, setup
 dependencies = ['click', 'boto', 'bs4', 'beautifulsoup', 'requests', 'py']
 
 setup(name='keyme',
-      version='0.5.0',
+      version='0.5.1',
       description='Google SAML STS login library',
       url='http://github.com/wheniwork/keyme',
       author='Richard Genthner',
