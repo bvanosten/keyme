79db3d4462ceb7b5dd80636ef29580b3f6f8c439
Richard Genthner <richard.genthner@wheniwork.com>
2017-06-29 13:25:06 -0400

    updated for python 3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages, setup
 dependencies = ['click', 'boto3', 'beautifulsoup4', 'requests', 'py']
 
 setup(name='keyme',
-      version='0.7.0',
+      version='0.7.1',
       description='Google SAML STS login library',
       url='http://github.com/wheniwork/keyme',
       author='Richard Genthner',
